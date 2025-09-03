96746bb6199649bd48208c5b2106712199351c8a
zhushengqian <3309997659@qq.com>
2025-09-04 07:27:45 +0800

    feat: 初始化 AWS S3 上传项目
    - 创建 API 和 Web两个模块
    - 添加基本的控制器和视图
    - 配置应用属性和静态资源
    - 创建测试用例
    - 添加 IDE 和构建工具配置

---
+++ aws-s3-upload-api/src/main/java/com/zsq/awss3uploadapi/AwsS3UploadApiApplication.java
@@ -0,0 +1,13 @@
+package com.zsq.awss3uploadapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsS3UploadApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsS3UploadApiApplication.class, args);
+    }
+
+}
---
+++ aws-s3-upload-api/src/test/java/com/zsq/awss3uploadapi/AwsS3UploadApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zsq.awss3uploadapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsS3UploadApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
